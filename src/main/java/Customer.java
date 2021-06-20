56cb1be9764af31f0f54509ad77501566e174ed5
hmidassi <hedy.midassi@gmail.com>
2021-06-20 15:23:38 +0200

    Initialized project

---
+++ src/main/java/Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+    private String name;
+
+    private CustomerType customerType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+}
---
+++ src/main/java/FabricPriceComputation.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal;
+
+public class FabricPriceComputation {
+    public BigDecimal computePrice(Customer customer, Fabric fabric, Integer length){
+        return BigDecimal.ZERO;
+    }
+}
